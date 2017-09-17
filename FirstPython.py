bc5a87fdb370d202049f5f0a0c9e687b8665475d
hiroki ueno <hiro1212ueno@gmail.com>
2017-09-17 15:12:01 +0900

    FirstPython.pyを修正
    ウインドウを開くように修正。

--- FirstPython.py
+++ FirstPython.py
@@ -1,1 +1,10 @@
-print("Hello World");
+# -*- coding:utf-8 -*-
+import tkinter #モジュールの読み込み
+
+window = tkinter.Tk()
+label = tkinter.Label(window, text = "サンプル")
+label.pack() # ---(1)
+button = tkinter.Button(window, text = "ボタンです。押しても何も起きません")
+button.pack()
+
+window.mainloop()
