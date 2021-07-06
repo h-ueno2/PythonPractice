669a3f5a9be1a6be3ac64d7c581c23353aaaa389
h-ueno2 <menbou1212@gmail.com>
2021-07-06 21:38:57 +0900

    update:  decorator.py

--- src/Plactice/chap09/decorator.py
+++ src/Plactice/chap09/decorator.py
@@ -3,14 +3,14 @@ from functools import lru_cache
 from time import sleep
 
 
-@lru_cache(maxsize=32)  # 32回分までキャッシュ
-def heavy_function(n):
-    sleep(1)  # 重たい処理
-    return n + 1
+# @lru_cache(maxsize=32)  # 32回分までキャッシュ
+# def heavy_function(n):
+#     sleep(1)  # 重たい処理
+#     return n + 1
 
 
-print(heavy_function(2))
-print(heavy_function(2))
+# print(heavy_function(2))
+# print(heavy_function(2))
 
 
 @dataclass(frozen=True)
@@ -23,3 +23,44 @@ class Fruit:
 
 apple = Fruit(name='apple',  price=128)
 print(apple)
+
+
+def deco1(f):
+    print('deco1 called')
+
+    def wrapper():
+        print('before exec')
+        v = f()  # ここで渡されてきた元の関数を実行する。
+        print('after exec')
+        return v
+    return wrapper
+
+
+@deco1
+def func():
+    print('exec')
+    return 1
+
+
+print(func.__name__)
+print(func())
+
+# 引数を受け取る場合にも対応させる
+
+
+def deco2(f):
+    def wrapper(*args, **kwargs):
+        print('before exec')
+        v = f(*args, **kwargs)  # ここで渡されてきた元の関数を実行する。
+        print('after exec')
+        return v
+    return wrapper
+
+
+@deco2
+def func(x, y):
+    print('exec')
+    return 1
+
+
+print(func(1, 2))
