0691c05473412b8ff8842c2c5b35a0a7463541d3
h-ueno2 <menbou1212@gmail.com>
2021-07-10 22:36:25 +0900

    add unittest sample

---
+++ src/Practice/chap12/booksearch_module.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+def booksearch():
+    # アプリケーションコード
+    return {}
+
+
+class BookSearchTest(unittest.TestCase):
+    def test_booksearch(self):
+        self.assertEqual({}, booksearch())
+
+
+if __name__ == '__name__':
+    unittest.main()
