f41dc8030cdf13e6ffe2534bf923306ce0088466
h-ueno2 <menbou1212@gmail.com>
2021-07-10 10:02:00 +0900

    descriptor update

--- src/Practice/chap09/descriptor.py
+++ src/Practice/chap09/descriptor.py
@@ -1,3 +1,6 @@
+from typing import Text
+
+
 class TextField:
     def __set_name__(self, owner, name):
         print(f'__set_name__ was called')
@@ -30,3 +33,24 @@ notebook.title = 'Notebook'
 
 book.title
 notebook.title
+
+
+class TextField:
+    # __get__のみ実装
+    def __init__(self, value) -> None:
+        if not isinstance(value, str):
+            raise AttributeError('must be str')
+        self.value = value
+
+    def __set_name__(self, owner, name):
+        print(f'__set_name__ was called')
+        print(f'{owner=}, {name=}')
+        self.name = name
+
+    def __get__(self, instance, owner):
+        print('__get__ was called')
+        return self.value
+
+
+class Book:
+    title = TextField('Python Practice Book')
