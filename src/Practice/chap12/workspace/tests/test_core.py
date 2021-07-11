c7bcd55c14f8d1fcd6b36ea31a6f80f6c174dfe1
h-ueno2 <menbou1212@gmail.com>
2021-07-11 12:06:23 +0900

    add mock test case

--- src/Practice/chap12/workspace/tests/test_core.py
+++ src/Practice/chap12/workspace/tests/test_core.py
@@ -1,6 +1,7 @@
 import pathlib
 import unittest
 from tempfile import TemporaryDirectory
+from unittest.mock import patch
 
 THUMBNAIL_URL = (
     'http://books.google.com/books/content'
@@ -18,8 +19,14 @@ class SaveThumbnailsTest(unittest.TestCase):
         # 一時ディレクトリを片づける
         self.tmp.cleanup()
 
-    def test_save_thumbnails(self):
+    @patch('booksearch.core.get_data')
+    def test_save_thumbnails(self, mock_get_data):
         from booksearch.core import Book
+
+        data_path = pathlib.Path(__file__).with_name('data')
+        mock_get_data.return_value = (
+            data_path / 'iLrrDwAAQBAJ_thumbnail.jpeg').read_bytes()
+
         book = Book({'id': '', 'volumeInfo': {
             'imageLinks':  {
                 'thumbnail': THUMBNAIL_URL
@@ -27,4 +34,6 @@ class SaveThumbnailsTest(unittest.TestCase):
 
         # 処理を実行し、ファイルが作成される事を確認する
         filename = book.save_thumbnails(self.tmp.name)[0]
+
+        mock_get_data.assert_called_with(THUMBNAIL_URL)
         self.assertTrue(pathlib.Path(filename).exists())
