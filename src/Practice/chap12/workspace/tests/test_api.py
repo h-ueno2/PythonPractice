68ae5e506cc9856647e3d0f686ede7930136b95f
h-ueno2 <menbou1212@gmail.com>
2021-07-11 17:43:48 +0900

    update test case

--- src/Practice/chap12/workspace/tests/test_api.py
+++ src/Practice/chap12/workspace/tests/test_api.py
@@ -1,4 +1,8 @@
 import unittest
+import sys
+import json
+from io import StringIO
+from unittest.mock import patch, MagicMock
 
 
 class BuildUrlTest(unittest.TestCase):
@@ -8,6 +12,22 @@ class BuildUrlTest(unittest.TestCase):
         expected = 'https://www.googleapis.com/books/v1/volumes?q=python'
         actual = build_url({'q': 'python'})
         self.assertEqual(expected, actual)
+
+    def test_build_url_multi(self):
+        from booksearch.api import build_url
+        base = 'https://www.googleapis.com/books/v1/volumes?'
+
+        params = (
+            (f'{base}q=python', {'q': 'python'}),
+            (f'{base}q=python&maxResults=1',
+             {'q': 'python', 'maxResults': 1}),
+            (f'{base}q=python&langRestrict=en', {
+             'q': 'python', 'langRestrict': 'en'}),
+        )
+        for expected, param in params:
+            with self.subTest(**param):
+                actual = build_url(param)
+                self.assertEqual(expected, actual)
 
     def test_build_url_empty_param(self):
         from booksearch.api import build_url
@@ -21,3 +41,23 @@ class BuildUrlTest(unittest.TestCase):
         expected = 'https://www.googleapis.com/books/v1/volumes'
         actual = build_url({})
         self.assertEqual(expected, actual, msg='このテストは失敗します。')
+
+    @unittest.skip('this isa skip test example')
+    def test_nothing_skip(self):
+        pass
+
+    @unittest.skipIf(sys.version_info > (3, 6), 'this is a skipIf test')
+    def test_nothing_skipIf(self):
+        pass
+
+    def test_get_json(self):
+        from booksearch.api import get_json
+        with patch('booksearch.api.request.urlopen') as mock_urlopen:
+            expected_response = {'id': 'test'}
+            fp = StringIO(json.dumps(expected_response))
+
+            mock = MagicMock()
+            mock.__enter__.return_value = fp
+            mock_urlopen.return_value = mock
+            actual = get_json({'q': 'python'})
+            self.assertEqual(expected_response, actual)
--- src/Practice/chap12/workspace/tests/test_core.py
+++ src/Practice/chap12/workspace/tests/test_core.py
@@ -2,6 +2,7 @@ import pathlib
 import unittest
 from tempfile import TemporaryDirectory
 from unittest.mock import patch
+from urllib.error import URLError
 
 THUMBNAIL_URL = (
     'http://books.google.com/books/content'
@@ -37,3 +38,11 @@ class SaveThumbnailsTest(unittest.TestCase):
 
         mock_get_data.assert_called_with(THUMBNAIL_URL)
         self.assertTrue(pathlib.Path(filename).exists())
+
+    def test_get_books_no_connection(self):
+        from booksearch.core import get_books
+
+        with patch('socket.socket.connect') as mock:
+            mock.return_value = None
+            with self.assertRaisesRegex(URLError, 'urlopen error'):
+                get_books(q='python')
